17afb48f01395580b73ff52f380d8a41a1b5986b
Nguyễn Nguyên Bảo <040802.nguyenbao@gmail.com>
2023-12-08 00:59:06 +0700

    web ver 0.8
    đơn hàng đã xác nhận

--- QL_DatVeXe_Web/QL_DatVeXe/QL_DatVeXe/Controllers/HoaDonController.cs
+++ QL_DatVeXe_Web/QL_DatVeXe/QL_DatVeXe/Controllers/HoaDonController.cs
@@ -36,5 +36,32 @@ namespace QL_DatVeXe.Controllers
             }
             return View(cxn);
         }
+
+        public ActionResult HoaDonDaXacNhan()
+        {
+            var user = Session["user"] as string;
+            if (string.IsNullOrEmpty(user))
+                Session["user"] = string.Empty;
+
+            ChoXacNhan cxn = new ChoXacNhan();
+            cxn.LstCTHD = new List<CHITIETHOADON>();
+            cxn.lstHoaDon = new List<HOADON>();
+
+            var lsthoadon = db.HOADONs.Where(t => t.KHACHHANG.TENKH == user && t.TRANGTHAI == true).ToList();
+            cxn.lstHoaDon = lsthoadon;
+            if (lsthoadon.Count > 0)
+            {
+                for (int i = 0; i < lsthoadon.Count; i++)
+                {
+                    var lstcthd = db.CHITIETHOADONs.Where(t => t.MAHD == lsthoadon[i].MAHD).ToList();
+                    for (int j = 0; j < lstcthd.Count; j++)
+                    {
+                        var cthd = db.CHITIETHOADONs.SingleOrDefault(t => t.MAVE == lstcthd[j].MAVE && t.MAHD == lstcthd[j].MAHD);
+                        cxn.LstCTHD.Add(cthd);
+                    }
+                }
+            }
+            return View(cxn);
+        }
     }
 }
